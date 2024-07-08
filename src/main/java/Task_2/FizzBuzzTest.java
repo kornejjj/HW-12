1feef47b033ba7ff74f4c6faeabf469e741dd526
kornejjj <165379747+kornejjj@users.noreply.github.com>
2024-07-08 19:26:42 +0200

    new2

--- src/main/java/Task_2/FizzBuzzTest.java
+++ src/main/java/Task_2/FizzBuzzTest.java
@@ -2,6 +2,57 @@ package Task_2;
 
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CountDownLatch;
+
+public class FizzBuzzTest {
+
+    static int NUMBER = 30;
+    static Queue<String> toPrinting = new ConcurrentLinkedQueue<>();
+    static CountDownLatch latch = new CountDownLatch(4);
+
+    public static void main(String[] args) {
+
+        NumberChecker B = new NumberChecker(toPrinting, NUMBER);
+        NumberChecker C = new NumberChecker(toPrinting, NUMBER);
+        NumberChecker D = new NumberChecker(toPrinting, NUMBER);
+
+        B.start();
+        C.start();
+        D.start();
+
+        Thread A = new Thread(() -> {
+            if (fizz(NUMBER)){
+                toPrinting.add("fizz");
+            }else {
+                toPrinting.add(String.valueOf(NUMBER));
+            }
+            latch.countDown();
+        });
+        A.start();
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        toPrinting.forEach(System.out::println);
+    }
+
+    public static boolean fizz(int n){
+        return n % 3 == 0;
+    }
+}
+
+
+
+
+
+
+/* package Task_2;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class FizzBuzzTest {
 
@@ -10,26 +61,26 @@ public class FizzBuzzTest {
 
     public static void main(String[] args) {
 
-        NumberChecker A = new NumberChecker(toPrinting, NUMBER);
         NumberChecker B = new NumberChecker(toPrinting, NUMBER);
         NumberChecker C = new NumberChecker(toPrinting, NUMBER);
         NumberChecker D = new NumberChecker(toPrinting, NUMBER);
-        A.start();
+
         B.start();
         C.start();
         D.start();
 
-        for (int i = 1; i <= NUMBER; i++) {
-            A.setNumber(i);
-            A.fizz();
-            B.buzz();
-            C.fizzbuzz();
-            D.number();
-            while (true) {
-                if (A.isProcessed() && B.isProcessed() && C.isProcessed() && D.isProcessed()) {
-                    break;
-                }
+        Thread A = new Thread(() -> {
+            if (fizz(NUMBER)){
+                toPrinting.add("fizz");
+            }else {
+                toPrinting.add(String.valueOf(NUMBER));
             }
-        }
+        });
+        A.start();
+
+
     }
-}
+    public static boolean fizz(int n){
+        return n % 3 == 0;
+    }
+}*/
