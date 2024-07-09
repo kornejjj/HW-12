1b342abcac89b67760fdb37e38f0e750b517a0cb
kornejjj <165379747+kornejjj@users.noreply.github.com>
2024-07-09 19:26:32 +0200

    new3

--- src/main/java/Task_2/FizzBuzzTest.java
+++ src/main/java/Task_2/FizzBuzzTest.java
@@ -12,37 +12,62 @@ public class FizzBuzzTest {
 
     public static void main(String[] args) {
 
-        NumberChecker B = new NumberChecker(toPrinting, NUMBER);
-        NumberChecker C = new NumberChecker(toPrinting, NUMBER);
-        NumberChecker D = new NumberChecker(toPrinting, NUMBER);
-
-        B.start();
-        C.start();
-        D.start();
-
-        Thread A = new Thread(() -> {
-            if (fizz(NUMBER)){
+        Thread fizzThread = new Thread(() -> {
+            if (fizz(NUMBER)) {
                 toPrinting.add("fizz");
-            }else {
+            }
+            latch.countDown();
+        });
+
+        Thread buzzThread = new Thread(() -> {
+            if (buzz(NUMBER)) {
+                toPrinting.add("buzz");
+            }
+            latch.countDown();
+        });
+
+        Thread fizzBuzzThread = new Thread(() -> {
+            if (fizzBuzz(NUMBER)) {
+                toPrinting.add("fizzbuzz");
+            }
+            latch.countDown();
+        });
+
+        Thread numberThread = new Thread(() -> {
+            if (!fizz(NUMBER) && !buzz(NUMBER) && !fizzBuzz(NUMBER)) {
                 toPrinting.add(String.valueOf(NUMBER));
             }
             latch.countDown();
         });
-        A.start();
+
+        fizzThread.start();
+        buzzThread.start();
+        fizzBuzzThread.start();
+        numberThread.start();
 
         try {
             latch.await();
         } catch (InterruptedException e) {
+            //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
 
         toPrinting.forEach(System.out::println);
     }
 
-    public static boolean fizz(int n){
-        return n % 3 == 0;
+    public static boolean fizz(int n) {
+        return n % 3 == 0 && n % 5 != 0;
+    }
+
+    public static boolean buzz(int n) {
+        return n % 5 == 0 && n % 3 != 0;
+    }
+
+    public static boolean fizzBuzz(int n) {
+        return n % 3 == 0 && n % 5 == 0;
     }
 }
+
 
 
 
