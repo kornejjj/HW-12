b392d4fcc4fd6f70f74e2e91504263e645978edf
kornejjj <165379747+kornejjj@users.noreply.github.com>
2024-07-12 15:07:30 +0200

    new5

--- src/main/java/Task_2/FizzBuzzTest.java
+++ src/main/java/Task_2/FizzBuzzTest.java
@@ -1,6 +1,111 @@
 package Task_2;
 
+
 import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class FizzBuzzTest {
+
+    static Queue<String> toPrinting = new ConcurrentLinkedQueue<>();
+    static final int LIMIT = 50;
+    static AtomicInteger currentNumber = new AtomicInteger(1);
+
+    public static void main(String[] args) {
+
+        CountDownLatch latch = new CountDownLatch(4);
+
+        CountDownLatch finalLatch = latch;
+        Thread fizzThread = new Thread(() -> {
+            while (true) {
+                int number = currentNumber.get();
+                if (number > LIMIT) break;
+                if (fizz(number) && currentNumber.compareAndSet(number, number + 1)) {
+                    toPrinting.add("fizz");
+                    finalLatch.countDown();
+                }
+            }
+        });
+
+        CountDownLatch finalLatch1 = latch;
+        Thread buzzThread = new Thread(() -> {
+            while (true) {
+                int number = currentNumber.get();
+                if (number > LIMIT) break;
+                if (buzz(number) && currentNumber.compareAndSet(number, number + 1)) {
+                    toPrinting.add("buzz");
+                    finalLatch1.countDown();
+                }
+            }
+        });
+
+        CountDownLatch finalLatch2 = latch;
+        Thread fizzBuzzThread = new Thread(() -> {
+            while (true) {
+                int number = currentNumber.get();
+                if (number > LIMIT) break;
+                if (fizzBuzz(number) && currentNumber.compareAndSet(number, number + 1)) {
+                    toPrinting.add("fizzbuzz");
+                    finalLatch2.countDown();
+                }
+            }
+        });
+
+        CountDownLatch finalLatch3 = latch;
+        Thread numberThread = new Thread(() -> {
+            while (true) {
+                int number = currentNumber.get();
+                if (number > LIMIT) break;
+                if (!fizz(number) && !buzz(number) && !fizzBuzz(number) && currentNumber.compareAndSet(number, number + 1)) {
+                    toPrinting.add(String.valueOf(number));
+                    finalLatch3.countDown();
+                }
+            }
+        });
+
+        fizzThread.start();
+        buzzThread.start();
+        fizzBuzzThread.start();
+        numberThread.start();
+
+        while (currentNumber.get() <= LIMIT) {
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            toPrinting.forEach(System.out::println);
+            toPrinting.clear();
+            latch = new CountDownLatch(4);
+        }
+
+        try {
+            fizzThread.join();
+            buzzThread.join();
+            fizzBuzzThread.join();
+            numberThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean fizz(int n) {
+        return n % 3 == 0 && n % 5 != 0;
+    }
+
+    public static boolean buzz(int n) {
+        return n % 5 == 0 && n % 3 != 0;
+    }
+
+    public static boolean fizzBuzz(int n) {
+        return n % 3 == 0 && n % 5 == 0;
+    }
+}
+
+
+/*import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -72,4 +177,4 @@ public class FizzBuzzTest {
     public static boolean fizzBuzz(int n) {
         return n % 3 == 0 && n % 5 == 0;
     }
-}
+}*/
